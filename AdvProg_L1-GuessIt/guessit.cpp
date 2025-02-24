64132bac1952ba195aa8aa7a3f68f2f6dd9d596b
missos1 <135939925+missos1@users.noreply.github.com>
2025-02-24 18:13:39 +0700

    re edit getAnswer

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -49,8 +49,8 @@ string getAnswer(int number, int randomNumber) {
     ***/
     
     string answer;
-    if (number < randomNumber) answer = "Your number is higher.";
-    else if (number > randomNumber) answer = "Your number is lower.";
+    if (number > randomNumber) answer = "Your number is higher.";
+    else if (number < randomNumber) answer = "Your number is lower.";
     else if (number == randomNumber) answer = "Congratulation! You win.";
 
     return answer;
